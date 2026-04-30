bca149f0eddf7120efee8121086163a31cde7b6f
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-04-30 02:48:41 +0000

    Boolean stuffs

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,10 +5,26 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
-        
-        let a = 18
-        let b = 284
-        let c = a+b
+    
+        print("Set an 'A' Value (Hint: try 21!)")
+        let a = Int(readLine()!)!
+        print("Set a 'B' Value (Hint: try 22!)")
+        let b = Int(readLine()!)!
+        let c = a+b 
+
+        let bool1 = c > b
+        let bool2 = a < b && a > 20
+        let bool3 = (b % 2) == 0 || (a % 2) != 0
+        let bool4 = (c % 2) != 0 || c > 50
+
+        print("Boolean 1 returns: \(bool1)")
+        print("Boolean 2 returns: \(bool2)")
+        print("Boolean 3 returns: \(bool3)")
+        print("Boolean 4 returns: \(bool4)")
+        let x = bool1 == true && bool2 == true && bool3 == true && bool4 == true
+
+        print("The answer is \(x)")
+
 
     }
 }
