33c24df0fdcdff779451deba2c381da53bf6cf06
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-03 22:18:12 +0000

    Did the task for booleans stuffs

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,26 +4,21 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    
-        print("Set an 'A' Value (Hint: try 21!)")
-        let a = Int(readLine()!)!
-        print("Set a 'B' Value (Hint: try 22!)")
-        let b = Int(readLine()!)!
-        let c = a+b 
+        print("Enter Your Age:")
 
-        let bool1 = c > b
-        let bool2 = a < b && a > 20
-        let bool3 = (b % 2) == 0 || (a % 2) != 0
-        let bool4 = (c % 2) != 0 || c > 50
+        var userInput = readLine()!
+        let age = Int(userInput)!
 
-        print("Boolean 1 returns: \(bool1)")
-        print("Boolean 2 returns: \(bool2)")
-        print("Boolean 3 returns: \(bool3)")
-        print("Boolean 4 returns: \(bool4)")
-        let x = bool1 == true && bool2 == true && bool3 == true && bool4 == true
+        print("Enter The Password")
+        userInput = readLine()!
 
-        print("The answer is \(x)")
+
+        let isOldEnough = age >= 18
+        let knowsSecret = userInput == "2007 BMW E91 320i"
+        let canEnterBar = isOldEnough && knowsSecret
+
+        print("Can Enter Bar: \(canEnterBar)")
+
 
 
     }
