7fc1146c2c3293f1ac06eceab1763403af23a67b
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-10 23:11:33 +0000

    Begun final task (not anywhere near finiished lololo)

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,6 +6,10 @@ struct SwiftPlayground {
     static func main() {
 
         let numbers = [4, 72, 8, 101, 24]
+
+        numbers.forEach { number in
+            number
+        }
         }
 
     }
