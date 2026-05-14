9e7fdb7df78c993ebac86b7c7b285cf9bd2e1271
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-14 02:17:02 +0000

    ignore

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,6 +9,29 @@ struct SwiftPlayground {
         var userRemove = false
         while userAppend {
             print ("Type the names of people you would like to invite to the party:")
+            let userInput = 
+            switch userInput{
+                case "q": //Quit
+                    print("Thank you. Your order will be:")
+                    selectedTickets.forEach { ticket in
+                        print(ticket)
+                    }
+                    print("")
+                    print("The final cost will be $\(cost)")
+                    isRunning = false
+                    case "c": //Child ticket
+                    cost = cost + 8
+                    selectedTickets = selectedTickets + ["Child Ticket"]
+                    print("A child ticket has been added. The total price of your tickets wil be: $\(cost)")
+                    case "a": //Adult ticket
+                    cost = cost + 12
+                    selectedTickets = selectedTickets + ["Adult Ticket"]
+                    print("An adult ticket has been added. The total price of your tickets wil be: $\(cost)")
+                    case "s": //Child ticket
+                    cost = cost + 18
+                    selectedTickets = selectedTickets + ["Senior Ticket"]
+                    print("A senior ticket has been added. The total price of your tickets wil be: $\(cost)")
+            }
         }
         }
 
