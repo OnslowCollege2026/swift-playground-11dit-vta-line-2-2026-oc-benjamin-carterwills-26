c82b6cbc6189e58580b3268761770ba0c3cc26a7
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-26 21:33:06 +0000

    Moved Comments

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,13 +4,17 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        //Task 1
-
-        let pricePerItem: Double = 19.99 // The price per each item.
-        let quantity: Double = 3.0 // The total amount of items bought.
-        let subTotal: Double = (pricePerItem * quantity) //The total cost of all items prior to GST tax.
-        let tax: Double = (pricePerItem * quantity * 0.15) // The total tax cost of all items, which is 15% of the subtotal
-        let totalPrice: Double = (subTotal + tax) // The subtotal plus the GST tax cost, as calculated in the previous statement.
+        //Task 1 - 
+        // The price per each item.
+        let pricePerItem: Double = 19.99 
+        // The total amount of items bought.
+        let quantity: Double = 3.0 
+        //The total cost of all items prior to GST tax.
+        let subTotal: Double = (pricePerItem * quantity)
+        // The total tax cost of all items, which is 15% of the subtotal
+        let tax: Double = (pricePerItem * quantity * 0.15) 
+        // The subtotal plus the GST tax cost, as calculated in the previous statement.
+        let totalPrice: Double = (subTotal + tax) 
 
         print("Price per item: $\(pricePerItem)")
         print("Quantity: \(quantity)")
@@ -18,11 +22,15 @@ struct SwiftPlayground {
         print("Tax (15%): $\(tax)")
         print("Total: $\(totalPrice)")
 
-        //Task 2
-        let rectLength: Double = 12.5 // The length of the rectangle.
-        let rectWidth: Double = 8 // The width of the rectangle.
-        let rectArea: Double = (rectWidth * rectLength) // Calculate the area of the rectangle by width times height.
-        let rectPerimeter: Double = 2 * (rectLength + rectWidth) //Calculate the perimter by adding the width and length together * 2
+        //Task 2 - Rectangle 
+        // The length of the rectangle.
+        let rectLength: Double = 12.5 
+        // The width of the rectangle.
+        let rectWidth: Double = 8 
+        // Calculate the area of the rectangle by width times height.
+        let rectArea: Double = (rectWidth * rectLength) 
+        //Calculate the perimter by adding the width and length together * 2
+        let rectPerimeter: Double = 2 * (rectLength + rectWidth) 
 
         print("The length of the rectangle is \(rectLength) units.")
         print("The width of the rectangle is \(rectWidth) units.")
