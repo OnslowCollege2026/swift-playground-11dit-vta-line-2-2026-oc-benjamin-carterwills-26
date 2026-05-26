867e495d691ca9e0d46663ac2a85c8acaa923efc
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-26 21:24:59 +0000

    Magic Numbers Task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,28 +4,31 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        //Task 1
 
-    var age: Int? = nil
+        let pricePerItem: Double = 19.99 // The price per each item.
+        let quantity: Double = 3.0 // The total amount of items bought.
+        let subTotal: Double = (pricePerItem * quantity) //The total cost of all items prior to GST tax.
+        let tax: Double = (pricePerItem * quantity * 0.15) // The total tax cost of all items, which is 15% of the subtotal
+        let totalPrice: Double = (subTotal + tax) // The subtotal plus the GST tax cost, as calculated in the previous statement.
 
-    while age == nil {
-        print("Enter your age:")
-        
-        if let input = readLine(), let enteredAge = Int(input) {
-            if enteredAge < 0 {
-                print("Age cannot be negative. Enter a valid age.")
-            } else if enteredAge < 18 {
-                print("You must be 18 or older. Enter a valid age.")
-            } else {
-                age = enteredAge
-            }
-        } else {
-            print("Invalid input. Enter a valid integer for age.")
-        }}
+        print("Price per item: $\(pricePerItem)")
+        print("Quantity: \(quantity)")
+        print("Subtotal: $\(subTotal)")
+        print("Tax (15%): $\(tax)")
+        print("Total: $\(totalPrice)")
 
+        //Task 2
+        let rectLength: Double = 12.5 // The length of the rectangle.
+        let rectWidth: Double = 8 // The width of the rectangle.
+        let rectArea: Double = (rectWidth * rectLength) // Calculate the area of the rectangle by width times height.
+        let rectPerimeter: Double = 2 * (rectLength + rectWidth) //Calculate the perimter by adding the width and length together * 2
 
-    print("Valid age entered: \(age!)")
+        print("The length of the rectangle is \(rectLength) units.")
+        print("The width of the rectangle is \(rectWidth) units.")
+        print("The area of the rectangle is \(rectArea) square units.")
+        print("The perimeter of the rectangle is \(rectPerimeter) units.")
 
-        
 
     }
 
