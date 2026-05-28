81dc5df12414265873b1f452c36717968b31ad78
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-05-28 02:58:28 +0000

    wotk on it later'

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,16 +4,42 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let isRunning = true
-        while isRunning{
-            
-        }
-        let ticketCost: Double = 17.5
-        let foodCost: Double = 8
-        let drinkCost: Double = 12.0
+        var ticketCostInput: Bool
+        var foodCostInput: Bool
+        var drinkCostInput: Bool
+        var personCountInput: Bool
+
+        let ticketCost: Double
+        let ticketCostMax = 100
+
+        let foodCost: Double
+        let foodCostMax = 1000
+
+        let drinkCost: Double
+        let drinkCostMax = 1000
+
         let perPersonCost: Double = (ticketCost + foodCost + drinkCost)
-        let personCount: Double = 5
+        let personCount: Double
         let total = (perPersonCost * personCount)
+
+        ticketCostInput = true
+
+        while ticketCostInput{
+            if let input = readLine(), let number = Int(input) {
+                print("Success! The ticket cost is is \(number)")
+
+                ticketCostInput = false
+                foodCostInput = true
+
+            } 
+            else if number > ticketCostMax || number <= 0{
+                
+            }
+            else {
+                print("Invalid input. Please enter a valid whole number.")
+            }
+        }
+        
 
         print("The total food cost for a party of \(personCount) will be\(foodCost * personCount)")
         print("The total drink cost for a party of \(personCount) will be\(drinkCost * personCount)")
