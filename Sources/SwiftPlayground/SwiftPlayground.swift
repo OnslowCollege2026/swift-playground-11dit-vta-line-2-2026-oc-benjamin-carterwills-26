61aa8c45b909d22f90763ffb4ca4fa99099096b1
oc-benjamin-carterwills-26 <benjamin.carterwills@student.onslow.school.nz>
2026-06-28 22:48:27 +0000

    Done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -30,7 +30,7 @@ struct SwiftPlayground {
             //Calls the Double Checker funtion to make sure the input is a Double, below the maximum and above the minimum
             let maxInput = checkDouble(minimum: minimumDailyLimit, maximum: maximumDailyLimit)
             //Prints out what the user set the maximum screen time limit to, then introduces them to the next part of the program.
-            print("Screen Time Maximum Set To: \(maxInput). Let's record the actual hours you spend on your device.").
+            print("Screen Time Maximum Set To: \(maxInput). Let's record the actual hours you spend on your device.")
 
             //repeats the loop for each day in the week array.
             days.forEach{ day in
@@ -40,22 +40,25 @@ struct SwiftPlayground {
                 //repeats this loop for each app in the apps array.
                 apps.forEach { app in
                     print("\(app):")
-                    //Calls the Double Check function to get user input, while making sure it is a Double, above the minimum app time and below the maximum app time, then adds that input to the day total.
+                    //Calls the Double Check function to get user input to see how many hours they spent on each app, while making sure it is a Double, above the minimum app time and below the maximum app time, then adds that input to the day total.
                     dayTotal += checkDouble(minimum: minimumAppTime, maximum: maximumAppTime)
                 }
                 //Prints the total hours the user spent doomscrolling on that day.
                 print("Overall on \(day) you spent \(dayTotal) hours doom scrolling.")
+                //Adds the day total to the week total. 
                 totalWeekTime += dayTotal
                 }
 
-            //Makes a daily average by dividing the total week time by the amount of days in the week. I useed days.count to remove magic numbers.
+            //Makes a daily average by dividing the total week time by the amount of days in the week. I useed days.count to remove magic numbers, but natually it is 7.
             let dailyAverage = totalWeekTime / Double(days.count)
-            //Makes an apply average by dividing the daily average by the amount of apps in the 
+            //Makes an per app average by dividing the daily average by the amount of apps in the apps array.
             let appAverage = dailyAverage / Double(apps.count)
+            //fancy formatting.
             print("=---------------------------------------------------------------------=")
             print("CONCLUSION")
             print("=---------------------------------------------------------------------=")
             print("This week, you spent a total of \(totalWeekTime) hours doom scrolling. This results to an average of \(dailyAverage) hours per day, and an average of \(appAverage) hours per app.")
+            //Tells the user wether or not they have gone over or above the inputted daily screentime limit.
             if (dailyAverage > maxInput){
                 print("Overall, you have gone OVERTIME on your screen limit!")
             }
@@ -63,21 +66,24 @@ struct SwiftPlayground {
                 print("Overall, you have RESPECTED your screen time limit! Good Job! :)")
             }
             print("=---------------------------------------------------------------------=")
+            //ends the application.
             isRunning = false
 
 
     }
 
-
+    //This function is used as an input function. It gets an input from the user, checks if it is Double, and if it is equal to or greater than the specified minimum, and less than or equal to the maximum.
     func checkDouble(minimum: Double, maximum: Double) -> Double{
+        //This boolean allows the input function to loop when the user nputs an invalid input.
         var functionRunning: Bool = true
         while functionRunning{
+            //If the usersinput is a Double, and fits the minimum and maximum, then return the input to the function call.
             if let userInput = readLine(), let targetInput = Double(userInput), targetInput >= minimum, targetInput <= maximum{
                 functionRunning = false
                 return targetInput
             }
             else{
-                print("Please enter a valid number between \(minimum) and \(maximum)")
+                print("Please try again. You must enter a valid number between \(minimum) and \(maximum)")
             }
         }
         
